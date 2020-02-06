e70901accb1f7669c2fcb5fed383a7a0cf363f64
yairdavidmercado <yairdavidmercado@gmail.com>
2020-02-06 09:48:18 -0500

    comunicado externo finalizado

--- php/conexion.php
+++ php/conexion.php
@@ -2,6 +2,6 @@
 	define ('DB_HOST','localhost'); //Host de postgresql (puede ser otro)
 	define ('DB_USER','postgres'); //Usuario de postgresql (puede ser otro)
 	define ('DB_PASS','admin'); //Password de postgresql (puede ser otro)
-	define ('DB_NAME','copia'); //Database de postgresql (puede ser otra)
+	define ('DB_NAME','IndicadoresEV'); //Database de postgresql (puede ser otra)
 	define ('DB_PORT','5432'); //Puerto de postgresql (puede ser otro)
 ?>
